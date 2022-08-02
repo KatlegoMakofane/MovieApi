21227137a48408018e97e316b0db17181b4a243b
Katlego Makofane <katlegomakofane3@gmail.com>
2022-08-02 12:39:21 +0200

    commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, ScrollView } from "react-native";
+import { useState, useEffect } from "react";
+import { fetchMovies } from "./Api/index";
+import { TextInput } from "react-native-paper";
+import MovieCard from "./Moviecard";
+import { StatusBar } from "react-native";
+import {Appbar} from "react-native-paper";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [movies, setMovies] = useState([])
+  const [searchMovie, setSearchMovie] = useState("prison break");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const getMovies = async () => {
+    setMovies(await fetchMovies(searchMovie));
+    setSearchMovie("");
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <>
+      {Object.keys(movies).length > 0 && (
+        <View>
+          <StatusBar barStyle="dark-content" />
+          <Appbar>
+           <Appbar.Content title="Movie app"></Appbar.Content>
+          </Appbar>
+          <TextInput
+            placeholder="Search your movies here"
+            value={searchMovie}
+            onChangeText={(text) => setSearchMovie(text)}
+            left={<TextInput.Icon name="magnify" />}
+            onSubmitEditing={getMovies}
+          />
+          <ScrollView>
+            {movies.Search.map((movie, i) => (
+              <MovieCard movie={movie} key={i} />
+            ))}
+          </ScrollView>
+        </View>
+      )}
+    </>
+  );
+            }
